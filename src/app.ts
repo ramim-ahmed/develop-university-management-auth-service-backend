a6590a2d295d04913ac5be35e58d56dcd404e60f
ramimcodes <ramimahmedraj2@gmail.com>
2023-06-04 02:43:37 +0600

    [DONE] setup eslint, prettier, huky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ const app: Application = express()
 // middlware setup
 app.use(cors()) // set origin policy
 app.use(express.json()) // parser
-app.use(express.urlencoded({ extendd: true })) // any data parser
+app.use(express.urlencoded({ extended: true })) // any data parser
 // application middlware
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
