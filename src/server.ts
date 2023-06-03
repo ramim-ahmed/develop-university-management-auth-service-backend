5c527e45dde204f5d0c620bdb3980c94350fe6de
ramimcodes <ramimahmedraj2@gmail.com>
2023-06-04 03:01:11 +0600

    [DONE] create user interface and model

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './app/config'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 async function bootstrap() {
   try {
-    await mongoose.connect(config.DATABASE_URL as string)
-    console.log('Database connected successfully!!!')
+    await mongoose.connect(config.DATABASE_URL as string);
+    console.log('Database connected successfully!!!');
     app.listen(config.PORT, () => {
-      console.log(`server listening on port ${config.PORT}`)
-    })
+      console.log(`server listening on port ${config.PORT}`);
+    });
   } catch (error) {
-    console.log(`internal server error ${error}`)
+    console.log(`internal server error ${error}`);
   }
 }
 
-bootstrap()
+bootstrap();
